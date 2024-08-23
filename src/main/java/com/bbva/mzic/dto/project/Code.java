a05d7daac030dfe4d9335f57df3925d34c03e2f8
Patricia-Cuevas <cuevasdpatricia@gmail.com>
2024-08-22 19:10:05 -0600

    primera practica

---
+++ src/main/java/com/bbva/mzic/dto/project/Code.java
@@ -0,0 +1,65 @@
+package com.bbva.mzic.dto.project;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+
+public class Costumer implements Serializable  {
+	private static final long serialVersionUID = 2931699728946643245L;
+
+	/* Attributes section for the DTO */
+
+	private int id;
+
+	/**
+	 * Get  
+	 */
+	public int getId() {
+		return this.id
+	}
+
+	public void setId(final int id) {
+		this.id = id;
+	}
+
+
+	/**
+	 * Indicates whether some other object is "equal to" this one.
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		if (obj == null) { return false; }
+		if (obj == this) { return true; }
+		if (obj.getClass() != getClass()) {
+			return false;
+		}
+		final Costumer rhs = (Costumer) obj;
+		return new EqualsBuilder().appendSuper(super.equals(obj))
+					.append(id, rhs.id)
+					.isEquals();
+	}
+
+	/**
+	 * Returns a hash code value for the object.
+	 */
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+			.append(this.id)
+			.toHashCode();
+	}
+
+	/**
+	 * Returns a string representation of the object.
+	 * It is important to OBFUSCATE the attributes that are sensitive to show in the representation.
+	 */
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this)
+			.append("id", id)
+			.toString();
+	}
+}
